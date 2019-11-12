a5807f3d98d571bb7523d14a1732e0feaabafac6
Matthew Honnibal <honnibal+gh@gmail.com>
2019-11-12 15:08:08 +0100

    Update German test

--- tests/lang/de/test_parser.py
+++ tests/lang/de/test_parser.py
@@ -17,7 +17,12 @@ def test_de_parser_depset(NLP, test_file):
                      "cj", "cm", "cp", "cvc", "da", "dm", "ep", "ju", "mnr",
                      "mo", "ng", "nk", "nmc", "oa", "oc", "og", "op", "par",
                      "pd", "pg", "ph", "pm", "pnc", "punct", "rc", "re", "rs",
-                     "sb", "sbp", "sp", "svp", "uc", "vo"])
+                     "sb", "sbp", "sp", "svp", "uc", "vo",
+                     # The 'dep' label is introduced by the frequency cutoff
+                     # during projectivisation. So we still need this, even
+                     # though it doesn't occur in the gold data.
+                     "dep"
+                     ])
     # fmt: on
     data_path = TEST_FILES_DIR / test_file
     if not data_path.exists():
