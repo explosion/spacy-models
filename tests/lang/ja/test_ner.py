13c18784531ac156f3e396417c59b888c7518250
Adriane Boyd <adrianeboyd@gmail.com>
2020-06-09 13:54:56 +0200

    Lower ja ner threshold

--- tests/lang/ja/test_ner.py
+++ tests/lang/ja/test_ner.py
@@ -10,7 +10,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,ents_f_threshold",
-    [("ja_gsd-ud-dev_sample.json",72)],
+    [("ja_gsd-ud-dev_sample.json",70)],
 )
 def test_ja_ner_corpus(NLP, test_file, ents_f_threshold):
     data_path = TEST_FILES_DIR / test_file
