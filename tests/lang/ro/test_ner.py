786063847561f6daa668adc47603248831e00aa4
Adriane Boyd <adrianeboyd@gmail.com>
2020-06-26 12:49:08 +0200

    Lower NER thresholds for ja and ro
    The NER test files are small enough that the evaluation is pretty
    unstable.

--- tests/lang/ro/test_ner.py
+++ tests/lang/ro/test_ner.py
@@ -10,7 +10,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,ents_f_threshold",
-    [("dev_ronec01_10.json",81)],
+    [("dev_ronec01_10.json",79)],
 )
 def test_ro_ner_corpus(NLP, test_file, ents_f_threshold):
     data_path = TEST_FILES_DIR / test_file
