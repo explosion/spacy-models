e65fe2c4add91c4f17ee88a6c78854f124417e30
Adriane Boyd <adrianeboyd@gmail.com>
2020-09-27 12:45:52 +0200

    Fix Spanish tests

--- tests/lang/es/test_parser.py
+++ tests/lang/es/test_parser.py
@@ -14,7 +14,7 @@ def test_es_parser_corpus(NLP, test_file, uas_threshold, las_threshold):
     data_path = TEST_FILES_DIR / test_file
     if not data_path.exists():
         raise FileNotFoundError("Test corpus not found", data_path)
-    examples = json_path_to_examples(data_path)
+    examples = json_path_to_examples(data_path, NLP)
     scores = NLP.evaluate(examples)
     assert scores["dep_uas"] > uas_threshold
     assert scores["dep_las"] > las_threshold
