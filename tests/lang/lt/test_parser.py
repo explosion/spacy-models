bec459bf8ea5368a91920e27f8e73c3bcc7c472f
Adriane Boyd <adrianeboyd@gmail.com>
2021-01-13 16:57:09 +0100

    Adjust thresholds for 3.0.0a1 models

--- tests/lang/lt/test_parser.py
+++ tests/lang/lt/test_parser.py
@@ -8,7 +8,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,uas_threshold,las_threshold",
-    [("lt_alksnis-ud-dev01_10.json", 0.60, 0.52)],
+    [("lt_alksnis-ud-dev01_10.json", 0.60, 0.51)],
 )
 def test_lt_parser_corpus(NLP, test_file, uas_threshold, las_threshold):
     data_path = TEST_FILES_DIR / test_file
