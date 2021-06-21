a1f29225655422cb68d201bd0e6b8be24333f9a9
Adriane Boyd <adrianeboyd@gmail.com>
2021-06-21 13:10:35 +0200

    Adjust ca ner

--- tests/lang/ca/test_ner.py
+++ tests/lang/ca/test_ner.py
@@ -8,7 +8,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,ents_f_threshold",
-    [("dev001_1.json", 0.67)],
+    [("dev001_1.json", 0.65)],
 )
 def test_ca_ner_corpus(NLP, test_file, ents_f_threshold):
     data_path = TEST_FILES_DIR / test_file
