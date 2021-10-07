1f17c3d71de3915fdfc1f0f2a879c5cdb02eeb79
Paul O'Leary McCann <polm@dampfkraft.com>
2021-10-07 06:41:27 +0000

    Fix issues with JA morph tests (#118)
    Two mistakes here on my part:
    
    1. I forgot that blank entries (marked with `*`) are omitted
    2. The inflection is still comma-separated, so it's a list in the
       MorphAnalysis. This should probably be changed to be hyphen
       delimited.

--- tests/lang/ja/test_morph.py
+++ tests/lang/ja/test_morph.py
@@ -4,8 +4,8 @@ import pytest
 @pytest.mark.parametrize(
     "word,morph",
     [
-        ("綜合", ("ソウゴウ", "*,*")),
-        ("縁側", ("エンガワ", "*,*")),
+        ("綜合", ("ソウゴウ", "")),
+        ("縁側", ("エンガワ", "")),
         ("新しい", ("アタラシイ", "形容詞,終止形-一般")),
         ("新しくない", ("アタラシク", "形容詞,連用形-一般")),
         ("やった", ("ヤッ", "五段-ラ行,連用形-促音便")),
@@ -17,4 +17,4 @@ def test_ja_morph(NLP, word, morph):
     reading, infl = morph
 
     assert reading == doc[0].morph.get("reading")[0]
-    assert infl == doc[0].morph.get("inflection")[0]
+    assert infl.split(",") == doc[0].morph.get("inflection")
