bdeab48908b1cd29ee08d63998525ceb60ec4b7a
Adriane Boyd <adrianeboyd@gmail.com>
2021-10-20 12:53:41 +0200

    Adjust ja parser thresholds

--- tests/lang/ja/test_parser.py
+++ tests/lang/ja/test_parser.py
@@ -8,7 +8,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,uas_threshold,las_threshold",
-    [("ja_gsd-ud-dev_sample.json", 0.88, 0.86)],
+    [("ja_gsd-ud-dev_sample.json", 0.88, 0.85)],
 )
 def test_ja_parser_corpus(NLP, test_file, uas_threshold, las_threshold):
     data_path = TEST_FILES_DIR / test_file
