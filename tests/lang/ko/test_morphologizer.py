3b5b6e932acac967e54b724f3a03c162856b19b1
Adriane Boyd <adrianeboyd@gmail.com>
2022-02-21 16:04:12 +0100

    Add draft tests for Korean (#132)

---
+++ tests/lang/ko/test_morphologizer.py
@@ -0,0 +1,19 @@
+import pytest
+from spacy.tokens import Doc
+from spacy.symbols import SPACE
+from pathlib import Path
+from ...util import evaluate_corpus
+
+
+TEST_FILES_DIR = Path(__file__).parent / "test_files"
+
+
+@pytest.mark.parametrize(
+    "test_file,pos_threshold,morph_threshold",
+    [("ko_kaist-ud-dev001_1.json", 0.92, 0.86)],
+)
+def test_ko_tagger_corpus(NLP, test_file, pos_threshold, morph_threshold):
+    data_path = TEST_FILES_DIR / test_file
+    evaluate_corpus(
+        NLP, data_path, {"pos_acc": pos_threshold, "morph_acc": morph_threshold}
+    )
