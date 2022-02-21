7aa586fa0aa67d713e6115ea0a48e9b6aa9b7355
Adriane Boyd <adrianeboyd@gmail.com>
2022-02-21 17:13:17 +0100

    Adjust threshold for sv lemmatizer test

--- tests/lang/sv/test_lemmatizer.py
+++ tests/lang/sv/test_lemmatizer.py
@@ -8,7 +8,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,lemma_threshold",
-    [("sv_talbanken-ud-dev01_1.json", 0.95)],
+    [("sv_talbanken-ud-dev01_1.json", 0.94)],
 )
 def test_sv_lemmatizer_corpus(NLP, test_file, lemma_threshold):
     data_path = TEST_FILES_DIR / test_file
