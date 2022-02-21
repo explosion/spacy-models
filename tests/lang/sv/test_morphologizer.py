a163833f98307f90443f6816299a7ff0b6a64b49
Adriane Boyd <adrianeboyd@gmail.com>
2022-02-21 16:53:28 +0100

    Adjust tests for sv_core_news_sm

--- tests/lang/sv/test_morphologizer.py
+++ tests/lang/sv/test_morphologizer.py
@@ -10,7 +10,7 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 @pytest.mark.parametrize(
     "test_file,pos_threshold,morph_threshold",
-    [("sv_talbanken-ud-dev01_1.json", 0.92, 0.86)],
+    [("sv_talbanken-ud-dev01_1.json", 0.94, 0.94)],
 )
 def test_sv_tagger_corpus(NLP, test_file, pos_threshold, morph_threshold):
     data_path = TEST_FILES_DIR / test_file
--- tests/lang/sv/test_ner.py
+++ tests/lang/sv/test_ner.py
@@ -7,8 +7,8 @@ TEST_FILES_DIR = Path(__file__).parent / "test_files"
 
 
 @pytest.mark.parametrize(
-    "test_file,ents_f_threshold", [("validation0001_1.json", 0.75)],
+    "test_file,ents_f_threshold", [("validation0001_1.json", 0.72)],
 )
-def test_sv_parser_corpus(NLP, test_file, ents_f_threshold):
+def test_sv_ner_corpus(NLP, test_file, ents_f_threshold):
     data_path = TEST_FILES_DIR / test_file
     evaluate_corpus(NLP, data_path, {"ents_f": ents_f_threshold})
